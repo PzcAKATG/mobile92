ac4ae6f2159a8d1497f5f561df69596b7faf76f2
TG <83356015@qq.com>
2020-02-10 22:43:31 +0800

    第一天

--- src/main.js
+++ src/main.js
@@ -2,6 +2,13 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import 'amfe-flexible/index.min.js'
+// import Vue from 'vue'
+import Vant from 'vant'
+import 'vant/lib/index.css'
+import '@/assets/css/global.less' // 引入全局的自定义样式  因为要覆盖vant的样式
+
+Vue.use(Vant)
 
 Vue.config.productionTip = false
 
